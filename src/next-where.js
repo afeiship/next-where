419f53f0852e0e9d6c3bbc439e18a95bd6bc2c1a
afeiship <1290657123@qq.com>
2017-09-13 16:30:07 +0800

    issue: optmize code

--- src/next-where.js
+++ src/next-where.js
@@ -4,26 +4,17 @@
   var nx = global.nx || require('next-js-core2');
 
   nx.where = function (inArray, inMap) {
-    var result = [];
     var pairs = nx.map(inMap,function(key,value){
       return { key:key, value: value };
     });
 
-    var eq = function(item){
+    return inArray.filter(function(item){
       var equals = pairs.filter(function(pair){
         return nx.path(item,pair.key) === pair.value;
       });
 
       return equals.length == pairs.length;
-    };
-
-    nx.each( inArray, function(_, item){
-      if(eq(item)){
-        result.push(item);
-      }
     });
-
-    return result;
   };
 
 
